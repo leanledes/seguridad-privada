33ace36a95b7db27a693bd2fbc2f7f94340284c2
Leandro Ledesma <leanledes2014@gmail.com>
2022-01-25 22:00:05 -0300

    cambios en envio y en index

--- envio/envio.php
+++ envio/envio.php
@@ -20,7 +20,7 @@
 		#Cambiar la ruta de refrescado cuando ya esté en el servidor
 		if($ready){
 			print "<script>alert('Mensaje enviado');</script>";
-            print "<meta http-equiv='refresh' content='0.001; http://localhost/seguridadrk/index.html'"; 
+            print "<meta http-equiv='refresh' content='0.001; https://leanledes.github.io/seguridad-privada/index.html'"; 
 		}else{
 			print "algo ocurrio!";
 		}
